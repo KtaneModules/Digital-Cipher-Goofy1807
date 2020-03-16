a813b7c080ea1d89d1d6590cea846de164864082
Brendan King <brendan@brendanking.com>
2020-03-15 21:38:47 -0400

    Optimized module (TP, newest modkit, etc.)

--- Assets/DigitalCipher.cs
+++ Assets/DigitalCipher.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -22,6 +24,8 @@ public class DigitalCipher : MonoBehaviour {
 
     private int timesPressed = 0;
 
+    private bool activated = false;
+
     static int moduleIdCounter = 1;
     int moduleId;
     private bool moduleSolved;
@@ -32,6 +36,7 @@ public class DigitalCipher : MonoBehaviour {
         {
             Buttons[i].OnInteract += ButtonPressed(i);
         }
+        BombModule.OnActivate += OnActivate;
     }
 
     private KMSelectable.OnInteractHandler ButtonPressed(int buttonPressed)
@@ -41,7 +46,7 @@ public class DigitalCipher : MonoBehaviour {
 
             Buttons[buttonPressed].AddInteractionPunch();
 
-            if (moduleSolved)
+            if (moduleSolved || !activated)
                 return false;
 
             if (Array.IndexOf(Alphanumeric, Char.ToLowerInvariant(outputTextCalculated[timesPressed])) == buttonPressed)
@@ -55,6 +60,7 @@ public class DigitalCipher : MonoBehaviour {
                     BombModule.HandlePass();
                     Debug.LogFormat(@"[Digital Cipher #{0}] Module was solved. Well done", moduleId);
                     moduleSolved = true;
+                    StartCoroutine(solveAnim());
                 }
             }
             else
@@ -71,8 +77,13 @@ public class DigitalCipher : MonoBehaviour {
 
         moduleId = moduleIdCounter++;
 
-        StartCoroutine(GenerateEverything());
+        //StartCoroutine(GenerateEverything());
 
+    }
+
+    void OnActivate()
+    {
+        StartCoroutine(GenerateEverything());
     }
 
     private IEnumerator GenerateEverything()
@@ -113,5 +124,125 @@ public class DigitalCipher : MonoBehaviour {
 
         Debug.LogFormat(@"[Digital Cipher #{0}] Input String: {1} Expected String: {2}", moduleId, inputText, outputTextCalculated);
 
+        activated = true;
+
+    }
+
+    private IEnumerator solveAnim()
+    {
+        yield return new WaitForSeconds(0.75f);
+        for (int i = 1; i <= 15; i++)
+        {
+            Input.text = Input.text.Substring(0, Input.text.Length - 1);
+            Output.text = Output.text.Substring(0, Output.text.Length - 1);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    //twitch plays
+    private bool cmdIsValid(string s)
+    {
+        string[] things = { "A","B","C","D","E","F","G","H","I" };
+        for(int i = 0; i < s.Length; i++)
+        {
+            string comp = s[i].ToString().ToUpper();
+            bool good = false;
+            for(int j = 0; j < things.Length; j++)
+            {
+                if (things[j].Equals(comp))
+                {
+
+                    good = true;
+                    break;
+                }
+            }
+            if(good == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} press <letters> [Presses the specified string of letters] | Valid letters are A-I";
+    #pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        string[] parameters = command.Split(' ');
+        if (Regex.IsMatch(parameters[0], @"^\s*press\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            if (parameters.Length > 2)
+            {
+                yield return "sendtochaterror Too many parameters!";
+            }
+            else if(parameters.Length == 2)
+            {
+                if (cmdIsValid(parameters[1]))
+                {
+                    for(int i = 0; i < parameters[1].Length; i++)
+                    {
+                        if (parameters[1][i].Equals('A') || parameters[1][i].Equals('a'))
+                        {
+                            Buttons[0].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('B') || parameters[1][i].Equals('b'))
+                        {
+                            Buttons[1].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('C') || parameters[1][i].Equals('c'))
+                        {
+                            Buttons[2].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('D') || parameters[1][i].Equals('d'))
+                        {
+                            Buttons[3].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('E') || parameters[1][i].Equals('e'))
+                        {
+                            Buttons[4].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('F') || parameters[1][i].Equals('f'))
+                        {
+                            Buttons[5].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('G') || parameters[1][i].Equals('g'))
+                        {
+                            Buttons[6].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('H') || parameters[1][i].Equals('h'))
+                        {
+                            Buttons[7].OnInteract();
+                        }
+                        else if (parameters[1][i].Equals('I') || parameters[1][i].Equals('i'))
+                        {
+                            Buttons[8].OnInteract();
+                        }
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                }
+                else
+                {
+                    yield return "sendtochaterror The specified string of letters to press '" + parameters[1] + "' is invalid!";
+                }
+            }
+            else if (parameters.Length == 1)
+            {
+                yield return "sendtochaterror Please specify the string of letters to press!";
+            }
+            yield break;
+        }
+    }
+
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        while (!activated) { yield return true; yield return new WaitForSeconds(0.1f); }
+        char[] chars = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
+        for(int i = timesPressed; i < outputTextCalculated.Length; i++)
+        {
+            Buttons[Array.IndexOf(chars, outputTextCalculated[i])].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
